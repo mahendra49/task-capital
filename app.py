b32034fb20273d6a917d03e1202f0279b44eb89f
mahendra <mahendrasuthar02@gmail.com>
2019-09-08 13:46:45 +0530

    final test

--- app.py
+++ app.py
@@ -6,9 +6,26 @@ app = Flask(__name__)
 
 capitals = processData()
 
+
 @app.route('/')
 def hello():
-    return "Hello World!"
+
+    return ("Hello DataGrokr.! The API is https://sample-python-app-task.herokuapp.com/capital?country=India")
+
+
+
+"""
+This API will give the capital of the given country
+
+The format is "https://sample-python-app-task.herokuapp.com/capital?country=%%%"
+Replace the %%% in above url with the country name
+
+Response is in the json with keys error and message
+
+if error is true then either country name is not provided or incorrect country name
+
+
+"""
 
 @app.route('/capital' , methods=['GET'])
 def getCapital():
